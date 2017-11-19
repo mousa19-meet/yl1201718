cc58bc4b8cd7411de22b7e193cff930601adbbcb
mousa <musabakri1@gmail.com>
2017-11-19 19:28:31 +0200

    random-color1

--- lab5.py
+++ lab5.py
@@ -3,17 +3,19 @@ from turtle import *
 colormode(255)
 import random
 
+red = random.randint(0,255)
+green = random.randint(0,255)
+blue = random.randint(0,255)
 
 class Square(Turtle):
 	def __init__(self,size):
 		Turtle.__init__(self)
 		self.shapesize(size)
 		self.shape("square")
-	def random_color(self,color):
-		self.colormode(color)
-		
+		self.color(red,green,blue)
+	def random_color(self):
+			print(red,green,blue)
 		
 ayo = Square(10)
-ayo.random_color(100)
-
+ayo.random_color()
 turtle.mainloop()
