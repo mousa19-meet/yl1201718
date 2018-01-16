93695a7cae8fb4a47124f29d4ee5cbb3fdfac383
mousa <musabakri1@gmail.com>
2018-01-16 19:57:15 +0200

    per-proj

--- agario.py
+++ agario.py
@@ -1,7 +1,48 @@
+from turtle import *
 import turtle
 import time
 import random
-import ball.py
+from ball import *
+colormode(255)
 
-turtle.tracer(0)
-ht()
+number_of_BALLS = 5 
+minimum_ball_radius = 10
+maximum_ball_radius = 100
+minimum_ball_dx = -5
+maximum_ball_dx = 5
+minimum_ball_dy = -5
+maximum_ball_dy = 5
+balls =[]
+
+screen_random1_x = int(-screen_width+maximum_ball_radius)
+screen_random2_x = int(screen_width-maximum_ball_radius)
+
+random_x = random.randint(screen_random1_x,screen_random2_x)
+print(random_x)
+
+screen_random1_y = int(-screen_height+maximum_ball_radius)
+screen_random2_y = int(screen_height-maximum_ball_radius)
+
+random_y = random.randint(screen_random1_y,screen_random2_y)
+print(random_y)
+
+random_dx = random.randint(minimum_ball_dx,maximum_ball_dx)
+print (random_dx)
+if random_dx == 0:
+	random_dx += 1
+
+random_dy = random.randint(minimum_ball_dy,maximum_ball_dy)
+print(random_dy)
+if random_dy == 0:
+	random_dy += 1
+
+radius = random.randint(minimum_ball_radius,maximum_ball_radius)
+print(radius)
+ 
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+print(color)
+
+ball1 = Ball(random_x,random_y,random_dx,random_dy,radius,color)
+
+
+mainloop()
--- ball.py
+++ ball.py
@@ -1,42 +1,52 @@
 from turtle import *
+import turtle
 import random
 import math
-#sqrt ( x2-x1 square ) + ( y2-y1 square)
+colormode(255)
 
-class ball(Turtle):
+running = True
+sleep = 0.0077
+screen_width =int(turtle.getcanvas().winfo_width()/2)
+screen_height = int(turtle.getcanvas().winfo_height()/2)
+
+
+class Ball(Turtle):
 	def __init__(self,x,y,dx,dy,r,color):
 		Turtle.__init__(self)
-		turtle.pu()
+		self.pu()
 		self.dx = dx
 		self.dy = dy
 		self.r = r
 		self.shapesize(r/10)
 		self.goto(x,y)
 		self.color(color)
+		self.shape("circle")
+
 
 
 	def move(self,width,height):
 		current_x = self.xcor()
-		new_x = current_x + dx 
+		new_x = current_x + self.dx 
 		current_y = self.ycor()
-		new_y = current_y + dy
-		right_side_ball = new_x + r
-		left_side_ball = new_x - r
-		top_side_ball = new_y + r
-		bottom_side_ball = new_y - r
+		new_y = current_y + self.dy
+		right_side_ball = new_x + self.r
+		left_side_ball = new_x - self.r
+		top_side_ball = new_y + self.r
+		bottom_side_ball = new_y - self.r
 		self.goto(new_x,new_y)
 
-		if top_side_ball > 300:
-			new_y = current_y - dy
 
-		elif bottom_side_ball < -300:
-			new_y = current_y + dy
+		if top_side_ball > screen_height:
+			new_y = current_y - self.dy
 
-		elif left_side_ball < -300:
-			new_x = current_x + dx
+		elif bottom_side_ball < -screen_height:
+			new_y = current_y + self.dy
 
-		elif right_side_ball > 300:
-			new_x = current_x - dx
+		elif left_side_ball < -screen_width:
+			new_x = current_x + self.dx
+
+		elif right_side_ball > screen_width:
+			new_x = current_x - self.dx
 
 #		def top(self):
 #			return self.ycor() + (0.5 *self.height)
@@ -52,5 +62,9 @@ class ball(Turtle):
 #a. bottom < b .bottom &
 #a.left < b.left :
 #return true 
+# my_ball = Ball(100,0,5,5,10 ,"red")
+# my_ball.goto(10,10)
+
+#sqrt ( x2-x1 square ) + ( y2-y1 square)
 
 
